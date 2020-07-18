a9278a5ccf29194dfd3f2588ea4f5d2f540535fc
sefirden <sefir.den@gmail.com>
2020-07-18 18:38:58 +0300

    new mechanik

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -12,19 +12,142 @@ public class Board : MonoBehaviour
 
     private BackgroundTile[,] allTiles;
     private int[,] numbers;
-    public GameObject[,] allDots; 
-    
+    public GameObject[,] allDots;
+    public GameObject[] CollectedNumbers;
+
+    private Vector2 startPosition, endPosition;
+    private GameObject tempObject;
+    private int index;
+    private int score;
+
+
     // Start is called before the first frame update
     void Start()
     {
         allTiles = new BackgroundTile[width, height];
         allDots = new GameObject[width, height];
         numbers = new int[width, height];
+        CollectedNumbers = new GameObject[9]; //максимальная длина цепочки - 9
+        index = 0;
+        score = 0;
         Shuffle();
         SetUp();
     }
 
-    private void Shuffle()
+    void Update()
+    {
+
+        endPosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y); //при каждом кадре считает последнюю позицию мышки
+
+        if (Input.GetMouseButtonDown(0))//клик кнопки мышки вниз
+        {
+            ClickSelect(); //ищем стартовую точку
+
+            //_lastTile = null;
+            // _selectedTiles.Clear();
+            //_checkInput = true;
+        }
+        else if(Input.GetMouseButton(0)) //когда мышь зажата
+        {
+            Debug.DrawRay(startPosition, endPosition); //для отладки
+
+                RaycastHit2D hit2 = Physics2D.Linecast(startPosition, endPosition); //кидаем лайнкаст каждый раз по апдейту из предыдущего тайла по положению курсора
+
+                if (hit2) //если что-то поймали лайнкастом
+                {
+                    if(Convert.ToInt32(tempObject.transform.tag) - Convert.ToInt32(hit2.transform.tag) == -1) //если текущая цифра больше предыдущей на 1
+                    {
+                    CollectedNumbers[index] = tempObject.transform.gameObject; //записываем первое значение в массив
+                    index++;
+
+
+                    tempObject.GetComponent<BoxCollider2D>().enabled = true; //включаем у предыдущего тайла колайдер
+
+                    Debug.Log(hit2.transform.tag);
+                
+                    hit2.transform.gameObject.GetComponent<BoxCollider2D>().enabled = false; //выключаем у текущего тайла колайдер, чтобы лайнкаст его не цеплял
+                    tempObject = hit2.transform.gameObject;//записываем последний тайл в темп, чтобы потом включить там колайдер
+
+                    endPosition = hit2.transform.position; //последнюю позицию ставим по центру тайла
+                    startPosition = endPosition; //начинаем новые лайнкасты с последнего положения мышки
+
+                    CollectedNumbers[index] = hit2.transform.gameObject; //записываем в массив
+                    index++;
+                }
+                    else
+                    {
+                    Debug.LogWarning("wrong number");
+                    }
+                }
+
+        }
+        
+
+        else if (Input.GetMouseButtonUp(0))//отпускаем кнопку мышки
+        {
+            // _checkInput = false;
+            Score(); //считаем очки
+            Destroy(); //удаляем собранные цифры
+
+
+            tempObject.GetComponent<BoxCollider2D>().enabled = true; //включаем коллайдер у последнего тайла
+            Array.Clear(CollectedNumbers,0,CollectedNumbers.Length); //обнуляем собранные цифры
+            index = 0;
+        }
+
+    }
+    private void Score()
+    {
+        int quantity = 0;
+        int tempScore = 0;
+
+        for (int i = 0; i < CollectedNumbers.Length; i++)
+        {
+            if (CollectedNumbers[i] != null && Convert.ToInt32(CollectedNumbers[i].tag) > 0)
+            {
+                tempScore += Convert.ToInt32(CollectedNumbers[i].tag);
+                quantity++;
+            }
+        }
+        score += tempScore * quantity;
+        Debug.LogWarning("Score: " + score);
+    }
+
+    private void Destroy()
+    {
+        for (int i = 0; i < CollectedNumbers.Length; i++)
+        {
+            Destroy(CollectedNumbers[i]);
+            CollectedNumbers[i] = null;
+        }
+    }
+
+
+
+    GameObject ClickSelect()
+    {
+        //Converting Mouse Pos to 2D (vector2) World Pos
+        Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
+
+        if (hit)
+        {
+            Debug.LogWarning("first click tag " + hit.transform.tag);
+
+            startPosition = hit.transform.position;
+            Debug.Log("first click position " + startPosition);
+
+
+            hit.transform.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            tempObject = hit.transform.gameObject;
+
+
+            return hit.transform.gameObject;
+        }
+        else return null;
+    }
+
+    private void Shuffle() //перемешиваем доску
     {
 
     for (int i = 0; i < width; i++)
@@ -50,7 +173,7 @@ public class Board : MonoBehaviour
         } 
     }
 
-    private void SetUp()
+    private void SetUp() //заполняем доску
     {
 
         for (int i = 0; i < width; i++)
--- Assets/Scripts/Dot.cs
+++ Assets/Scripts/Dot.cs
@@ -13,57 +13,24 @@ public class Dot : MonoBehaviour
     private Board board;
     private GameObject otherDot;
 
-    private Vector2 firstTouchPosition;
-    private Vector2 finalTouchPosition;
-    private Vector2 tempPosition;
     public float swipeAngle = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         board = FindObjectOfType<Board>();
-        targetX = (int)transform.position.x;
-        targetY = (int)transform.position.y;
         row = targetY;
         column = targetX;
     }
-
-    // Update is called once per frame
-    /*void Update()
+     
+    void Update()
     {
         targetX = column;
         targetY = row;
-        if(Mathf.Abs(targetX - transform.position.x) > .1)
-        {
-            //Move towards the target
-            tempPosition = new Vector2(targetX, transform.position.y);
-            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
-        }
-        else
-        {
-            //Directly set the position
-            tempPosition = new Vector2(targetX, transform.position.y);
-            transform.position = tempPosition;
-            board.allDots[column, row] = this.gameObject;
-        }
 
-        if (Mathf.Abs(targetY - transform.position.y) > .1)
-        {
-            //Move towards the target
-            tempPosition = new Vector2(transform.position.x, targetY);
-            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
-        }
-        else
-        {
-            //Directly set the position
-            tempPosition = new Vector2(transform.position.x, targetY);
-            transform.position = tempPosition;
-            board.allDots[column, row] = this.gameObject;
-        }
+    }
 
-
-    }*/
-
+    //--------------------------
     //OnMouseDown()
     //нажимаем мышку - записываем первую точку в список, при свайпе считаем угол и кидаем в ту сторону райкастер и проверяем "совместимость" цифр по порядку,
     //если совместимо записываем вторую точку в список, и уже при свайпе проверяем от этой точки как от начальной
@@ -72,67 +39,6 @@ public class Dot : MonoBehaviour
     //при отпускании мышки отдельным методом записываем очки - суммируем числа в списке [1;2;3;4] и множим на количество в списке (*4)
     //следующим отдельным методом убираем цифры с доски и заполняем пустое место
     //следкющий отдельный метод проверяет есть ли комбинации и и если их нет то конец игры
+    //--------------------------
 
-
-    private void OnMouseDown()
-    {
-        firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Debug.Log(firstTouchPosition);
-
-        //тут добавляем первый элемент в список
-    }
-
-    private void OnMouseUp()
-    {
-        finalTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        CalculateAngle();
-    }
-
-    void CalculateAngle()
-    {
-        swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * 180 / Mathf.PI;
-        Debug.Log(swipeAngle);
-       // MovePieces();
-        CollectNumbers();
-    }
-
-    //выделять цифру и добавлять в отдельный масив, и потом проверять правильный порядок цифр и если порядок ок, то засчитывать очки и очищать масив
-    private void CollectNumbers()
-    {
-        throw new NotImplementedException();
-    }
-
-
-
-    void MovePieces()
-    {
-        if (swipeAngle > -45 && swipeAngle <= 45 && column < board.width)
-        {
-            //Right Swipe
-            otherDot = board.allDots[column + 1, row];
-            otherDot.GetComponent<Dot>().column -= 1;
-            column += 1;
-        }
-        else if (swipeAngle > 45 && swipeAngle <= 135 && row < board.height)
-        {
-            //Up Swipe
-            otherDot = board.allDots[column, row + 1];
-            otherDot.GetComponent<Dot>().row -= 1;
-            row += 1;
-        }
-        else if ((swipeAngle > 135 || swipeAngle <= -135) && column > 0)
-        {
-            //Left Swipe
-            otherDot = board.allDots[column - 1, row];
-            otherDot.GetComponent<Dot>().column += 1;
-            column -= 1;
-        }
-        else if (swipeAngle < -45 && swipeAngle >= -135 && row > 0)
-        {
-            //Down Swipe
-            otherDot = board.allDots[column, row - 1];
-            otherDot.GetComponent<Dot>().row += 1;
-            row -= 1;
-        }
-    }
 }
